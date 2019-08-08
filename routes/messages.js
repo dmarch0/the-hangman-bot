e8a068d931cdfe782c30f939cdb415d975778d70
DaniilMarch <asmandian@gmail.com>
2019-08-08 17:07:38 +0300

    fixed game process func

--- routes/messages.js
+++ routes/messages.js
@@ -7,7 +7,7 @@ const gameData = {};
 
 router.post("/", (req, res, next) => {
   //commented code line to wash down the drain pipe pending errors in case errors overflow
-  return res.status(200).json({ ok: true });
+  //return res.status(200).json({ ok: true });
   const messageText = req.body.message.text;
   let response;
   //Если сообщение - команда, то ответить на команду
