14e0b01c1911f884e6cbf774bfb1a14dc343b8b1
DaniilMarch <asmandian@gmail.com>
2019-08-08 13:13:37 +0300

    doing commands with regex

--- routes/messages.js
+++ routes/messages.js
@@ -1,20 +1,30 @@
 const router = require("express").Router();
 const client = require("../bot-client");
 
+const availableCommands = [/^(\/test)/, /^(\/new)/];
+
 router.post("/", (req, res, next) => {
   const messageText = req.body.message.text;
-  const commands = req.body.message.entities.filter(
-    entitie => entitie.type === "bot_command"
-  );
-  console.log(commands);
-  client
-    .sendMessage(
-      req.body.message.chat.id,
-      `Your message text was ${messageText} and commands were ${commands.toString()}`
-    )
-    .promise()
-    .then(() => res.json({ ok: true }))
-    .catch(next);
+  const messageCommand = "";
+  for (let command of availableCommands) {
+    messageCommand += messageText.match(command)[0];
+  }
+  if (messageCommand) {
+    client
+      .sendMessage(
+        req.body.message.chat.id,
+        `Your message text was ${messageText} and command was ${messageCommand}`
+      )
+      .promise()
+      .then(() => res.json({ ok: true }))
+      .catch(next);
+  } else {
+    client
+      .sendMessage(req.body.message.chat.id, `No command found`)
+      .promise()
+      .then(() => res.json({ ok: true }))
+      .catch(next);
+  }
 });
 
 module.exports = router;
