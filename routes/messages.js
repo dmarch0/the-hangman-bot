e5002926e9b8c9d1c31d83d6a4ce3755883cf36b
DaniilMarch <asmandian@gmail.com>
2019-08-08 16:15:20 +0300

    trying to fix game process

--- routes/messages.js
+++ routes/messages.js
@@ -5,19 +5,21 @@ const availableCommands = require("../commands");
 const gameData = {};
 
 router.post("/", (req, res, next) => {
+  //commented code line to wash down the drain pipe in case too many errors
   //return res.status(200).json({ ok: true });
   const messageText = req.body.message.text;
   let response;
-  for (let commandKey in availableCommands) {
-    if (gameData[req.body.message.chat.id]) {
-      response = availableCommands.try.response(req.body.message, gameData);
-      break;
-    }
-    const command = availableCommands[commandKey];
-    if (command.regex.test(messageText)) {
-      response = command.response(req.body.message, gameData);
+  if (gameData[req.body.message.chat.id]) {
+    response = availableCommands.try.response(req.body.message, gameData);
+  } else {
+    for (let commandKey in availableCommands) {
+      const command = availableCommands[commandKey];
+      if (command.regex.test(messageText)) {
+        response = command.response(req.body.message, gameData);
+      }
     }
   }
+
   client
     .sendMessage(req.body.message.chat.id, response)
     .promise()
