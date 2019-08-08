98c666d31251fd528d51fca71e97dc0b4db67171
DaniilMarch <asmandian@gmail.com>
2019-08-08 16:21:55 +0300

    restart

--- routes/messages.js
+++ routes/messages.js
@@ -6,7 +6,7 @@ const gameData = {};
 
 router.post("/", (req, res, next) => {
   //commented code line to wash down the drain pipe in case too many errors
-  return res.status(200).json({ ok: true });
+  //return res.status(200).json({ ok: true });
   const messageText = req.body.message.text;
   let response = "Помогите, я ничего не понимаю!";
   if (gameData[req.body.message.chat.id]) {
